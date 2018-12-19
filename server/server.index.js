898f987bc6a92b48ff7c73db2647c37399347ed0
Manuel Minana <efficiel.technologie@gmail.com>
2018-12-19 10:21:42 +0100

    gql in a dedicated module

--- server/server.index.js
+++ server/server.index.js
@@ -1,37 +1,15 @@
 import express from 'express';
 import helmet from 'helmet';
 
-import graphqlHTTP from 'express-graphql';
-import { buildSchema } from 'graphql';
-
 import conf from './server.conf';
-
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+import gql from './server.gql';
 
 const app = express();
-app.use(helmet());
-
-app.use(
-  '/gql',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  }),
-);
-
 const host = 'localhost';
 const port = conf.API_GQL_PORT;
+
+app.use(helmet());
+app.use('/gql', gql);
 
 app.listen(port, host, () => {
   console.info(`listen ${port}`);
